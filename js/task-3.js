46afaf3054b01a2e24d221286d293215f67ab9b4
Katrin <kateyka1410@gmail.com>
2024-04-11 12:58:00 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,11 @@
-function getElementWidth(content, padding, border) {
-    const contentWidt = Number.parseFloat(content); 
-    const paddingWidth = Number.parseFloat(padding);
-    const borderWidth = Number.parseFloat(border);
-    const getElementWidt = contentWidt + 2 * paddingWidth + 2 * borderWidth;
-    return getElementWidt
+function checkForSpam(message) {
+    const hasSpamSale = message.toLowerCase();
+    return hasSpamSale.includes("spam") || hasSpamSale.includes("sale");
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter")); 
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
